4ca443e37eb38b7a1cf80d5589a6f744cdf79bee
Simon <simon.stark54@gmail.com>
2022-08-29 16:18:48 +0200

    first commit

---
+++ src/main/java/_1_cluedosbmvc_datelist/Application.java
@@ -0,0 +1,13 @@
+package _1_cluedosbmvc_datelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/_1_cluedosbmvc_datelist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package _1_cluedosbmvc_datelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
